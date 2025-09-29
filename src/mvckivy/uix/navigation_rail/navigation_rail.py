5093e3f51da2ae75440729b00794723e2571af37
Mihenb <bingin62@gmail.com>
2025-09-29 21:55:05 +0300

    Fix crash test app when you tap on button in navigation_rail.py

--- src/mvckivy/uix/navigation_rail/navigation_rail.py
+++ src/mvckivy/uix/navigation_rail/navigation_rail.py
@@ -59,38 +59,27 @@ class NavigationRailItem(MDNavigationRailItem, MVCBehavior):
 
 class NavigationRailItemIcon(MDNavigationRailItemIcon):
     def finish_ripple(self) -> None:
-        if self._doing_ripple and not self._finishing_ripple:
-            self._finishing_ripple = True
-            self._doing_ripple = False
-            Animation.cancel_all(self, "_ripple_rad")
-            self.fade_out()
+        super().finish_ripple()
 
     def fade_out(self, *args) -> None:
-        rc = self.ripple_color
-        if not self._fading_out:
-            self._fading_out = True
-            Animation.cancel_all(self, "ripple_color")
-            self.anim_complete()
+        Animation.cancel_all(self, "ripple_color")
+        self.anim_complete()
 
     def anim_complete(self, *args) -> None:
         """Fired when the "fade_out" animation complete."""
 
-        self._doing_ripple = False
-        self._finishing_ripple = False
-        self._fading_out = False
-
-        if not self.ripple_canvas_after:
-            canvas = self.canvas.before
+        has_navigation = self._navigation_rail is not None and self._navigation_item is not None
+        if has_navigation:
+            super().anim_complete(*args)
         else:
-            canvas = self.canvas.after
-
-        canvas.remove_group("circular_ripple_behavior")
-        canvas.remove_group("rectangular_ripple_behavior")
-
-        self.on_release()
+            super(MDNavigationRailItemIcon, self).anim_complete(*args)
+        if self.parent:
+            self.on_release()
 
     def on_release(self):
-        self.parent.dispatch("on_release")
+        parent = self.parent
+        if parent is not None:
+            parent.dispatch("on_release")
 
 
 class NavigationRailItemLabel(MDNavigationRailItemLabel):
@@ -107,22 +96,11 @@ class NavigationRailButton(
         self._last_parent = None
 
     def on_parent(self, widget, parent):
-        if parent is None and self._last_parent is not None:
-            self._last_parent.add_widget(self)
-            self.opacity = 1
-            anim = Animation(opacity=0, duration=0.5)
-            anim.bind(on_complete=self._remove_widget)
-            anim.start(self)
-        elif parent is not None:
-            self._last_parent = parent
-            self.opacity = 0
-            anim = Animation(opacity=1, duration=0.5)
-            anim.start(self)
-
-    def _remove_widget(self, *args):
-        lp = self._last_parent
-        self._last_parent = None
-        lp.remove_widget(self)
+        if parent is None:
+            return
+        self._last_parent = parent
+        self.opacity = 0
+        Animation(opacity=1, duration=0.5).start(self)
 
 
 class NavigationRailMenuButton(MDNavigationRailMenuButton, ButtonHoverBehavior):
